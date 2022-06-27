1f420e070cce1be664642244b308d468520adb26
Pavel Tagiltsev <pavel.tagiltsef@gmail.com>
2022-06-27 16:59:20 +0300

    Adding cors

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
-
-const PORT = process.env.PORT || 8080;
+import cors from "cors";
 
 dotenv.config();
 
+const PORT = process.env.PORT || 8080;
+const CORS_OPTIONS = {
+  origin: process.env.ALLOWED_ORIGIN || "http://localhost:3000",
+};
+
 const app = express();
+
+app.use(cors(CORS_OPTIONS));
 
 app.listen(PORT, (error) => {
   if (error) {
