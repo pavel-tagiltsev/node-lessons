a65d446c0c8c9b4771b04f1decf554e90d36c091
Pavel Tagiltsev <pavel.tagiltsef@gmail.com>
2022-06-28 11:41:48 +0300

    Creating user model

---
+++ app/controllers/auth.controller.js
@@ -0,0 +1,32 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+export async function signin(req, res) {
+  const user = await User.findOne("email", req.body.email);
+
+  if (!user) {
+    return res.status(404).send({ message: "User Not found." });
+  }
+
+  const passwordIsValid = +req.body.password === +user.password;
+
+  if (!passwordIsValid) {
+    return res.status(401).send({
+      accessToken: null,
+      message: "Invalid Password!",
+    });
+  }
+
+  const secret = "some secret value";
+
+  const token = jwt.sign({ id: user.id }, secret, {
+    expiresIn: 86400, // 24 hours
+  });
+
+  res.status(200).send({
+    userId: user.id,
+    name: user.name,
+    email: user.email,
+    accessToken: token,
+  });
+}
